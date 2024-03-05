9be8652152a09a586ac3a883709fe960eacbdf54
Zetong Yang <tomztyang@gmail.com>
2024-03-05 06:34:21 +0000

    fix bug

--- projects/mmdet3d_plugin/__init__.py
+++ projects/mmdet3d_plugin/__init__.py
@@ -5,7 +5,7 @@ from .core.evaluation.eval_hooks import CustomDistEvalHook
 from .core.hooks.ema import ExpMomentumEMAHook, LinearMomentumEMAHook
 from .datasets.pipelines import (
   PhotoMetricDistortionMultiViewImage, PadMultiViewImage, 
-  NormalizeMultiviewImage,  CustomCollect3D)
+  NormalizeMultiviewImage,  CustomCollect3D, RandomScaleImageMultiViewImage)
 from .models.utils import *
 from .models.opt.adamw import AdamW2
 from .bevformer import *
